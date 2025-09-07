89de688f70f351578e8680cd46eed0e7fef46ff7
Dominik Stempel <dominik.stempel7@gmail.com>
2025-09-07 20:26:22 +0200

    Fixed TAB for cookie.ts

--- src/ts/utils/cookie.ts
+++ src/ts/utils/cookie.ts
@@ -20,28 +20,27 @@ function runGtag(...args: any[]) {
 
 
 export const cookies = () => {
-    document.getElementById("cookie-banner-button")!.addEventListener("click", function() {
-        document.getElementById("cookie-banner")!.style.display='none';
+  document.getElementById("cookie-banner-button")!.addEventListener("click", function() {
+    document.getElementById("cookie-banner")!.style.display='none';
 
-        // "XXXXXXXXXX" is a place holder
         
-        let gtagScript = document.createElement("script");
-        gtagScript.async = true;
-        gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-7LTJ46JCWB";
-        document.head.appendChild(gtagScript);
+    let gtagScript = document.createElement("script");
+    gtagScript.async = true;
+    gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-7LTJ46JCWB";
+    document.head.appendChild(gtagScript);
 
-        gtagScript.onload = function() {
-            window.dataLayer = window.dataLayer || [];
-            function gtag(...args: any[]) {
-                window.dataLayer.push(args);
-            }
-            window.gtag = gtag;
-            gtag("js", new Date());
-            gtag("config", "G-7LTJ46JCWB", { "anonymize_ip": true });
+    gtagScript.onload = function() {
+      window.dataLayer = window.dataLayer || [];
+      function gtag(...args: any[]) {
+        window.dataLayer.push(args);
+      }
+      window.gtag = gtag;
+      gtag("js", new Date());
+      gtag("config", "G-7LTJ46JCWB", { "anonymize_ip": true });
 
-            gtagReady = true;
-            gtagQueue.forEach(args => window.gtag!(...args));
-            runGtag('event', 'page_view');
-        };
-    });
+      gtagReady = true;
+      gtagQueue.forEach(args => window.gtag!(...args));
+        runGtag('event', 'page_view');
+      };
+  });
 };
